ea378909eee906fbfb39ef9ad46cfa6102c013e5
jeffreymorales978 <jeffmorayo@gmail.com>
2022-10-25 19:51:24 -0500

    version9

---
+++ sumanum.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+using namespace std;
+
+int main ()
+{
+  float x,s=0.0;
+  int i=0,l;
+  cout<<"Ingrese el valor de l: ";
+  cin>>l;
+  do{
+
+  cout<<"Ingrese el valor de x: ";
+  cin>>x;
+  i=i+1;
+  s=s+x;
+
+
+  }while(i<l);
+  cout<<"La suma de los numero fue: "<<s<<endl;
+  return 0;
+
+
+
+
+
+}
